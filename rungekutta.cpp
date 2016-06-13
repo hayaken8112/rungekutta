199d028ffa5c1856b166638d667cdd6e9edf1f26
hayaken <hayashi8112@gmail.com>
2016-06-13 14:13:32 +0900

    二次配列追加

--- rungekutta.cpp
+++ rungekutta.cpp
@@ -1,51 +1,77 @@
-#include <iostream>
-#include <math.h>
+#include<iostream>
+#include<math.h>
+#include<vector>
 #define IMAX 100001
-double func(double x, double y)
+
+class Func 
 {
-    double dydx;
-    dydx = cos(x);
-    
-    return (dydx);
+public:
+    double operator()(double x, double y)
+        {
+            double dydx;
+            dydx = sin(x) * cos(x) - y * cos(x);
+            return dydx;
+        }
+};
+
+
+class RungeKutta 
+{
+public:
+    RungeKutta(int num_of_params, double target):m_num_of_params(num_of_params), m_target(target)
+        {
+            m_axis[0] = 0.0;
+            m_params.resize(m_num_of_params);
+            for(int i = 0; i < m_num_of_params; i++){
+                m_params[i].resize(IMAX);
+                m_params[i][0] = 0.0;
+            }
+            m_steps = 10000;
             
-}
+            
+            
+        }
+    template<class F>
+    void calc(F func)
+        {
+            double k1, k2, k3, k4;
+            double h = (m_target - m_axis[0]) / m_steps;
+            
 
-double rungeKutta(double x[], double y[], double final_x, int n, double (*func)(double, double))
-{
-    double h, f1, f2, f3, f4;
+            for (int i = 0; i < m_steps; i++){
+                k1 = h * func(m_axis[i], m_params[0][i]);
+                k2 = h * func(m_axis[i] + h / 2.0, m_params[0][i] + k1 / 2.0);
+                k3 = h * func(m_axis[i] + h / 2.0, m_params[0][i] + k2 / 2.0);
+                k4 = h * func(m_axis[i] + h, m_params[0][i] + k3);
 
-    h = (final_x - x[0]) / n;
-
-    for (int i = 0; i < n; i++){
-        f1 = h * func(x[i], y[i]);
-        f2 = h * func(x[i] + h/2, y[i] + f1/2);
-        f3 = h * func(x[i] + h/2, y[i] + f2/2);
-        f4 = h * func(x[i] + h, y[i] + f3);
-        y[i+1] = y[i] + (f1 + 2 * f2 + 2 * f3 + f4)/6;
-        x[i+1] = x[i] + h;
-        std::cout << x[i] << " " << y[i] << std::endl;
-    }
-    return y[n-1];
+                m_axis[i+1] = m_axis[i] + h;
+                m_params[0][i+1] = m_params[0][i] + 1.0 / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4);
+                std::cout << m_axis[i] << " " << m_params[0][i] << std::endl;
+            }
+        }
+private:
+    int m_num_of_params;
+    double m_axis[IMAX];
+    std::vector<std::vector<double>> m_params;
+    double m_target;
+    int m_steps;
     
-}
+    
+};
 
+    
 int main()
 {
-    double x[IMAX], y[IMAX];
-    double final_x;
-    int n;
 
-    x[0] = 0.0;
-    y[0] = 0.0;
-
-    final_x = 100.0;
-    n = 10000;
-
-    rungeKutta(x, y, final_x, n, func);
+    Func func;
+    RungeKutta rungekutta(1, 10.0);
     
+    rungekutta.calc(func);
 
-    
     return 0;
     
 }
 
+
+
+    
